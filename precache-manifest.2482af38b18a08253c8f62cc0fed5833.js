d43fc8231022aa78cd6dff75e982dabfc358baf4
nheingit <noah.hein@g.austincc.edu>
2020-12-22 10:22:06 -0600

    first commit

---
+++ precache-manifest.2482af38b18a08253c8f62cc0fed5833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04449cad114e68956a0847991a6a18b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b872344b101184b99d01",
+    "url": "/static/css/main.b5ae5d8e.chunk.css"
+  },
+  {
+    "revision": "15f3a52f08f55f8e1806",
+    "url": "/static/js/2.9dc8e09e.chunk.js"
+  },
+  {
+    "revision": "d2d3c99be8821c18e29b42be2a109444",
+    "url": "/static/js/2.9dc8e09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b872344b101184b99d01",
+    "url": "/static/js/main.c503b8ac.chunk.js"
+  },
+  {
+    "revision": "7a5d0b255e38991b78f2",
+    "url": "/static/js/runtime-main.0d78e5c8.js"
+  }
+]);
